5d0d483ef6ce0f44743880dea435300b4f0aec11
Leonardo Risco <leo99153@gmail.com>
2024-03-29 20:47:57 -0500

    Product's table service is not fullfilled.

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -13,7 +13,61 @@ export class ProductService {
     ){}
 
     async createProduct(product: CreateProductDto){
+        const newProduct = this.productRepository.findOne({
+            where: {
+                nombre: product.nombre
+            }
+        });
 
+        if (newProduct){
+            throw new HttpException(`Producto ya existente.`, 400);
+        } else {
+            const crearProducto = await this.productRepository.create(product);
+            return this.productRepository.save(crearProducto);
+        };
+    };
 
+    getProducts(){
+        return this.productRepository.find();
+    };
+
+    async getProduct(id: UUID){
+        const productFound = await this.productRepository.findOne({
+            where: {
+                id: id
+            }
+        });
+        if ( productFound ){
+            return productFound;
+        } else {
+            throw new HttpException(`El ID del producto buscado es inexistente.`,400);
+        }
+    };
+
+    async deleteProduct(id: UUID){
+        const productBuscado = await this.productRepository.findOne({
+            where: {
+                id: id
+            }
+        });
+        if (!productBuscado){
+            return new HttpException(`Producto a borrar no encontrado.`,400);
+        } else {
+            return this.productRepository.delete({id: id});
+        }
+    };
+
+    async updateProduct(id: UUID, product: UpdateProductDto){
+        const productBuscado = await this.productRepository.findOne({
+            where: {
+                id: id
+            }
+        });
+
+        if (!productBuscado){
+            return new HttpException(`Producto a actualizar no encontrado.`, 400);
+        } else {
+            return this.productRepository.update({id}, product);
+        }
     }
 }
