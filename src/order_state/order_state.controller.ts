df460b1528e4b4cfb088f74147ea5650fd27cac5
Leonardo Risco <leo99153@gmail.com>
2024-04-01 03:28:20 -0500

    order_state terminada

--- src/order_state/order_state.controller.ts
+++ src/order_state/order_state.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, ParseIntPipe, ParseUUIDPipe, Delete, Patch, Put } from '@nestjs/common';
+import { UUID } from 'crypto';
+import { OrderStateService } from './order_state.service';
+import { Order_State } from './order_state.entity';
+import { CreateOrderStateDto } from './dto/CreateOrderState.dto';
+import { UpdateOrderStateDto } from './dto/UpdateOrderState.dto';
+import { OrderService } from 'src/order/order.service';
 
 @Controller('order-state')
-export class OrderStateController {}
+export class OrderStateController {
+    constructor(private orderStateService: OrderStateService){}
+
+    @Post()
+    createOrderState(@Body() orderState: CreateOrderStateDto){
+        return this.orderStateService.createOrderState(orderState);
+    };
+
+    @Get()
+    getOrderStates(){
+        return this.orderStateService.getOrderStates();
+    };
+
+    @Get(':id')
+    getOrderState(@Param('id', ParseUUIDPipe) id: UUID){
+        return this.orderStateService.getOrderState(id);
+    };
+
+    @Delete(':id')
+    deleteOrderState(@Param('id', ParseUUIDPipe) id: UUID){
+        return this.orderStateService.deleteOrderState(id);
+    };
+
+    @Put(':id')
+    updateOrderState(@Param('id', ParseUUIDPipe) id: UUID, @Body() orderState: UpdateOrderStateDto){
+        return this.orderStateService.updateOrderState(id, orderState);
+    };
+};
--- src/order_state/order_state.service.ts
+++ src/order_state/order_state.service.ts
@@ -1,4 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order_State } from './order_state.entity';
+import { UUID } from 'crypto';
+import { Repository } from 'typeorm';
+import { CreateOrderStateDto } from './dto/CreateOrderState.dto';
+import { UpdateOrderStateDto } from './dto/UpdateOrderState.dto';
 
 @Injectable()
-export class OrderStateService {}
+export class OrderStateService {
+    constructor(@InjectRepository(Order_State) private orderStateRepository: Repository <Order_State>){}
+
+    async createOrderState(orderState: CreateOrderStateDto){
+        const verifyOrder = await this.orderStateRepository.findOne({
+            where: {
+                state: orderState.state
+            }
+        });
+
+        if (verifyOrder) {
+            throw new HttpException("",400);
+        } else {
+            const createOrder = this.orderStateRepository.create(orderState);
+            return this.orderStateRepository.save(createOrder);
+        };
+    };
+
+    getOrderStates(){
+        return this.orderStateRepository.find();
+    };
+
+    async getOrderState(id: UUID){
+        const verifyState = await this.orderStateRepository.findOne({
+            where: {
+                id: id
+            }
+        });
+
+        if (verifyState){
+            return verifyState;
+        } else {
+            throw new HttpException('This OrderState does not exist.', 400);
+        };
+    };
+
+    async deleteOrderState(id: UUID){
+        const orderVerify = await this.orderStateRepository.findOne({
+            where: {
+                id: id
+            }
+        });
+
+        if(orderVerify){
+            return this.orderStateRepository.delete({id});
+        } else {
+            throw new HttpException("OrderState could not be found.", 400);
+        }
+     };
+
+     async updateOrderState(id : UUID, orderState: UpdateOrderStateDto){
+        const stateVerify = await this.orderStateRepository.findOne({
+            where: {
+                id : id
+            }
+        }); 
+        if(stateVerify){
+            return this.orderStateRepository.update({id}, orderState)
+        } else {
+            throw new HttpException("This orderState was not found.", 400);
+        };
+     };
+};
