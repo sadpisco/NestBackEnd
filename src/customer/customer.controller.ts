02abf71393a7fe535c1ee23762b3701aeef1475b
Leonardo Risco <leo99153@gmail.com>
2024-03-31 19:12:22 -0500

    controller table has already added

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, ParseIntPipe, ParseUUIDPipe, Delete, Patch, Put } from '@nestjs/common';
+import { UUID } from 'crypto';
+import { Customer } from './customer.entity';
+import { CustomerService } from './customer.service';
+import { CreateCustomerDto } from './dto/CreateCustomer.dto';
+import { UpdateCustomerDto } from './dto/UpdateCustomer.dto';
 
 @Controller('customer')
-export class CustomerController {}
+export class CustomerController {
+    constructor(private customerService: CustomerService){}
+
+    @Post()
+    createCustomer(@Body() customer: CreateCustomerDto){
+        return this.customerService.createCustomer(customer);
+    };
+
+    @Get()
+    getCustomers(){
+        return this.customerService.getCustomers();
+    };
+
+    @Get(':id')
+    getCustomer(@Param('id', ParseUUIDPipe) id: UUID){
+        return this.customerService.getCustomer(id);
+    };
+
+    @Delete(':id')
+    deleteCustomer(@Param('id', ParseUUIDPipe) id: UUID){
+        return this.customerService.deleteCustomer(id);
+    };
+
+    @Put(':id')
+    updateCustomer(@Param('id', ParseUUIDPipe) id: UUID, @Body() customer:UpdateCustomerDto ){
+        return this.customerService.updateCustomer(id, customer);
+    };
+};
