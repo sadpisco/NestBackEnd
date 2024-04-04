8a8696e1b946969e6f21b154de677bedf56b42e5
Leonardo Risco <leo99153@gmail.com>
2024-04-04 15:35:47 -0500

    controllers finished

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, ParseIntPipe, ParseUUIDPipe, Delete, Put } from '@nestjs/common';
+import { Order } from './order.entity';
+import { UUID } from 'crypto';
+import { CreateOrderDto } from './dto/CreateOrder.dto';
+import { UpdateOrderDto } from './dto/UpdateOrder.dto';
+import { OrderService } from './order.service';
 
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+    constructor(private orderService: OrderService){}
+
+    @Post()
+    createOrder(@Body() order: CreateOrderDto){
+        return this.orderService.createOrder(order);
+    };
+
+    @Get()
+    getOrders(){
+        return this.orderService.getOrders();
+    };
+
+    @Get(':id')
+    getOrder(@Param('id', ParseUUIDPipe) id: UUID){
+        return this.orderService.getOrder(id);
+    };
+
+    @Delete(':id')
+    deleteOrder(@Param('id', ParseUUIDPipe) id: UUID){
+        return this.orderService.deleteOrder(id);
+    };
+
+    @Put(':id')
+    updateOrder(@Param('id', ParseUUIDPipe) id: UUID, @Body() order: CreateOrderDto){
+        return this.orderService.updateOrder(id, order);
+    };
+};
--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, ParseIntPipe, ParseUUIDPipe, Delete, Put } from '@nestjs/common';
+import { Payment } from './payment.entity';
+import { UUID } from 'crypto';
+import { PaymentService } from './payment.service';
+import { CreatePaymentDto } from './dto/CreatePayment.dto';
+import { UpdatePaymentDto } from './dto/UpdatePayment.dto';
 
 @Controller('payment')
-export class PaymentController {}
+export class PaymentController {
+    constructor(private paymentService: PaymentService){}
+
+    @Post()
+    createPayment(@Body() payment: CreatePaymentDto){
+        return this.paymentService.createPayment(payment);
+    };
+
+    @Get()
+    getPayments(){
+        return this.paymentService.getPayments();
+    };
+
+    @Get(':id')
+    getPayment(@Param('id', ParseUUIDPipe) id: UUID){
+        return this.paymentService.getPayment(id);
+    };
+
+    @Delete(':id')
+    deletePayment(@Param('id', ParseUUIDPipe) id: UUID){
+        return this.paymentService.deletePayment(id);
+    };
+
+    @Put(':id')
+    updatePayment(@Param('id', ParseUUIDPipe) id: UUID, @Body() payment:UpdatePaymentDto){
+        return this.paymentService.updatePayment(id, payment);
+    };
+};
